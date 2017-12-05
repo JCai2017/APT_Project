199ec4d67e002d5af14b8603a5bbbc048d5a8916
zero33542 <kaichih1013@gmail.com>
2017-12-05 15:42:15 -0600

    update agreement activity

--- Thread/app/src/main/java/ee382apt/thread/AgreementActivity.java
+++ Thread/app/src/main/java/ee382apt/thread/AgreementActivity.java
@@ -8,9 +8,15 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import cz.msebera.android.httpclient.Header;
+
 public class AgreementActivity extends AppCompatActivity {
     String email = null;
-
+    private String API_URL = "https://apt-fall2017.appspot.com/agree";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,20 +24,32 @@ public class AgreementActivity extends AppCompatActivity {
 
         email = getIntent().getStringExtra("email");
         Log.i("AgreeActivity", email);
-        //TODO: Check for agreement on database
     }
 
     public boolean checked(){
         CheckBox box = (CheckBox) this.findViewById(R.id.checkBox);
         return box.isChecked();
     }
-
-    //TODO: Store agreement to database
+    
     public void onClick(View view){
         if(checked()){
-            Intent intent = new Intent(this, MainHUBActivity.class);
-            intent.putExtra("email", email);
-            startActivity(intent);
+            RequestParams params = new RequestParams();
+            params.put("email", email);
+            AsyncHttpClient client = new AsyncHttpClient();
+            client.post(API_URL, params, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                    Log.w("async", "success!!!!");
+                    Toast.makeText(AgreementActivity.this, "Agree Successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(AgreementActivity.this, MainHUBActivity.class);
+                    intent.putExtra("email", email);
+                    startActivity(intent);
+                }
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                    Log.e("Posting_to_blob","There was a problem in retrieving the url : " + e.toString());
+                }
+            });
         }else{
             Toast.makeText(this, "Please check that you agree to the Terms of Service", Toast.LENGTH_LONG)
                 .show();
